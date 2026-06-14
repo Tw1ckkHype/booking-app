6ce09539238d1e0b1f8d955867ab5e7a8b73297b
tw1ckk <zetmaster13@gmail.com>
2026-06-15 02:48:52 +0800

    Fix baseURL and add navigation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   app: {
-    baseURL: '/booking-app/',
+    baseURL: '/booking-app/',  
     head: {
       title: 'Бронирование ресурсов',
       meta: [
